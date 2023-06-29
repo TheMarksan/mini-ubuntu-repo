7ddbd4e7d9bfee69d8deb291c229f5fda047ec25
TheMarksan <marcosmelodossantos.com@gmail.com>
2023-06-29 13:05:03 +0000

    mini-git-repo (100% termux)

---
+++ README.md
@@ -0,0 +1,1 @@
+# mini-ubuntu-repo
---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    
+    int v1, v2;
+    double media;
+
+    scanf("%d %d", &v1, &v2);
+
+    media = (v1 + v2)/2.0;
+
+    printf("MEDIA = %.1lf\n", media);
+
+    return 0;
+
+
+
+}
